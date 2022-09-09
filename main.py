606fe0c5ad94504141a8148c3b64f45ff52a4f21
sanavila <rute.leone@hotmail.com>
2022-09-09 16:16:05 -0300

    hits and misses score

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ print("*********************************")
 numero_secreto = random.randrange(1, 101)
 total_de_tentativas = 0 
 rodada = 1
+pontos = 1000
 
 print("Qual nível de dificuldade?")
 print("(1) Fácil (2) Médio (3) Difícil")
@@ -27,18 +28,20 @@ for rodada in range(1, total_de_tentativas + 1):
     chute = int(chute_str)
     
     if(chute < 1 or chute > 100):
-        print("Você deve digitar um número entre 1 e 100")
+        print("Você deve digitar um número entre 1 e 100: ")
         continue
 
     maior = chute > numero_secreto 
     menor = chute < numero_secreto
 
     if(numero_secreto == chute):
-            print("Você acertou!")
+            print("Você acertou e fez {} pontos!".format(pontos))
             break
     else:
         if(maior):
             print("Você errou! Seu número é maior que o número secreto")
         elif(menor):
             print("Você errou! Seu número é menor que o número secreto")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 print("Fim de jogo!")
