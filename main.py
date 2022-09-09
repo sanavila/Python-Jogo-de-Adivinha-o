073c600bb9ce400807ee896eb887e7a0817fae1d
sanavila <rute.leone@hotmail.com>
2022-09-09 15:52:43 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import random
+
+print("*********************************")
+print("Bem vindo ao jogo de adivinhação!")
+print("*********************************")
+
+numero_secreto = random.randrange(1, 101)
+total_de_tentativas = 0 
+rodada = 1
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite um número entre 1 e 100:")
+    print("Você digitou:", chute_str) 
+    chute = int(chute_str)
+    
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100")
+        continue
+
+    maior = chute > numero_secreto 
+    menor = chute < numero_secreto
+
+    if(numero_secreto == chute):
+            print("Você acertou!")
+            break
+    else:
+        if(maior):
+            print("Você errou! Seu número é maior que o número secreto")
+        elif(menor):
+            print("Você errou! Seu número é menor que o número secreto")
+print("Fim de jogo!")
